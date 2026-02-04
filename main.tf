e215c8d714fe29e450c3f2d3770b661034f13934
Maissen Belgacem <maissen.developer500@gmail.com>
2026-02-05 00:11:05 +0100

    Import a module to handle vpc creation

--- main.tf
+++ main.tf
@@ -8,3 +8,22 @@ terraform {
 
   required_version = ">= 1.1"
 }
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 6.6"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.private_subnets_cidrs
+  public_subnets  = var.public_subnets_cidrs
+
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
--- variables.tf
+++ variables.tf
@@ -16,6 +16,36 @@ variable "vpc_name" {
   default     = "pfe_vpc"
 }
 
+variable "vpc_azs" {
+  type        = list(string)
+  description = "the availability zones of subnets within the vpc"
+}
+
+variable "private_subnets_cidrs" {
+  type        = list(string)
+  description = "the list of cidr blocks for the private subnets"
+  default     = []
+}
+
+variable "public_subnets_cidrs" {
+  type        = list(string)
+  description = "the list of cidr blocks for the public subnets"
+  default     = []
+}
+
+variable "enable_dns_hostnames" {
+  type        = bool
+  description = "enable or desable dns hostnames withing a vpc"
+  default     = true
+}
+
+variable "enable_dns_support" {
+  type        = bool
+  description = "enable or desable dns support withing a vpc"
+  default     = true
+}
+
+
 variable "igw_name" {
   type        = string
   description = "the name of your igw"
