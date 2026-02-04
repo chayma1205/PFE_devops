8abe1109a4a0226f7db67c832f881421194e854d
Maissen Belgacem <maissen.developer500@gmail.com>
2026-02-05 00:17:39 +0100

    Clean project vars and outputs

--- variables.tf
+++ variables.tf
@@ -44,34 +44,3 @@ variable "enable_dns_support" {
   description = "enable or desable dns support withing a vpc"
   default     = true
 }
-
-
-variable "igw_name" {
-  type        = string
-  description = "the name of your igw"
-  default     = "my_igw"
-}
-
-variable "route_table_name" {
-  type        = string
-  description = "the name of your route tabe"
-  default     = "public_rt"
-}
-
-variable "public_subnet_1_az" {
-  type        = string
-  description = "subnet az"
-  default     = "us-east-1a"
-}
-
-variable "public_subnet_1_cidr" {
-  type        = string
-  description = "public subnet cidr block"
-  default     = "10.0.1.0/24"
-}
-
-variable "public_subnet_1_name" {
-  type        = string
-  description = "public subnet name"
-  default     = "public_1"
-}
