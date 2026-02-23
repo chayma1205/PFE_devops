00d639a168fb86e0f44b6534b5a5dd94367ea2d8
Maissen <maissen.developer500@gmail.com>
2026-02-23 21:13:35 +0100

    Add 'remote_backend_init' to initialize remote backend s3 & DynamoDB

--- project/variables.tf
+++ project/variables.tf
@@ -1,20 +1,4 @@
 #########
-# REMOTE STATE
-#########
-
-variable "state_bucket_name" {
-  type        = string
-  description = "The remote state S3 bucket name, must be globally unique"
-}
-
-variable "dynamodb_table_name" {
-  type        = string
-  description = "The name of dynamoDB table to hold the state locking"
-  default     = "state_lock"
-}
-
-
-#########
 # VPC
 #########
 
