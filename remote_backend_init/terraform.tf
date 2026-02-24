3c547e05a0a26e23bc38b80ac188e4c300351afc
Maissen <maissen.developer500@gmail.com>
2026-02-24 14:41:55 +0100

    Update init_backend_init config

---
+++ remote_backend_init/terraform.tf
@@ -0,0 +1,22 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.30"
+    }
+  }
+
+  required_version = ">= 1.1"
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Environment = "dev"
+      ManagedBy   = "Terraform"
+    }
+  }
+}
--- remote_backend_init/variables.tf
+++ remote_backend_init/variables.tf
@@ -2,6 +2,13 @@
 # REMOTE STATE
 #########
 
+variable "aws_region" {
+  type        = string
+  description = "The region for the provisioned aws s3 and DynamoDB"
+  default     = "us-east-1"
+}
+
+
 variable "state_bucket_name" {
   type        = string
   description = "The remote state S3 bucket name, must be globally unique"
