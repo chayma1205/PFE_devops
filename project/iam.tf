a8e578db2c85cc95e4ec44acf473be98d0d6e17b
Maissen <maissen.developer500@gmail.com>
2026-02-28 19:38:45 +0100

    Create a role for ECS tasks using a community module

--- project/iam.tf
+++ project/iam.tf
@@ -23,3 +23,30 @@ module "iam_bastion" {
     AWSSecretsManagerClientReadOnlyAccess = "arn:aws:iam::aws:policy/AWSSecretsManagerClientReadOnlyAccess"
   }
 }
+
+module "iam_ecs_backend" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role"
+  version = "6.4.0"
+
+  name                    = "${var.vpc_name}-ecs-task-role"
+  use_name_prefix         = false
+  create_instance_profile = false
+
+  description = "This role is for ECS tasks, using this custom role in order to avoid creating a new role for each task definition by the ecs module"
+
+  trust_policy_permissions = {
+    TrustRoleAndServiceToAssume = {
+      actions = ["sts:AssumeRole"]
+      principals = [
+        {
+          type        = "Service"
+          identifiers = ["ecs-tasks.amazonaws.com"]
+        }
+      ]
+    }
+  }
+
+  policies = {
+    AmazonECSTaskExecutionRolePolicy = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  }
+}
