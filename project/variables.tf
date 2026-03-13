79832f0c8aa2bc864a981a610cc841ff87491b69
Maissen <maissen.developer500@gmail.com>
2026-03-13 02:02:51 +0100

    Clear unused variables

--- project/variables.tf
+++ project/variables.tf
@@ -96,11 +96,6 @@ variable "bastion_storage_size" {
   default     = 30
 }
 
-variable "bastion_allowed_db_port" {
-  type        = number
-  description = "The port of database service hosted in the bastion instance"
-}
-
 #########
 # ALB
 #########
@@ -112,50 +107,6 @@ variable "ecs_frontend_tasks_port" {
 variable "ecs_backend_tasks_port" {
   type        = number
   description = "The port of ecs backend tasks"
-}
-
-#########
-# ASG
-#########
-
-variable "asg_name" {
-  type        = string
-  description = "ASG name"
-}
-
-variable "asg_min_size" {
-  type        = number
-  description = "Minimum instances"
-}
-
-variable "asg_max_size" {
-  type        = number
-  description = "Maximum instances"
-}
-
-variable "asg_desired_capacity" {
-  type        = number
-  description = "Desired instances"
-}
-
-variable "asg_launch_template_name" {
-  type        = string
-  description = "name of the launch template"
-}
-
-variable "asg_launch_template_description" {
-  type        = string
-  description = "description for the launch template"
-}
-
-variable "asg_image_id" {
-  type        = string
-  description = "ami id to use for the instances"
-}
-
-variable "asg_instance_type" {
-  type        = string
-  description = "ec2 instance type"
 }
 
 #########
@@ -202,27 +153,6 @@ variable "backend_service_desired_tasks" {
   type        = number
   description = "the desired tasks number for backend tasks"
 }
-
-variable "backend_task_db_user" {
-  description = "Database username for the backend application"
-  type        = string
-}
-
-variable "backend_task_db_password" {
-  description = "Database password for the backend application"
-  type        = string
-}
-
-variable "backend_task_db_port" {
-  description = "Database port number"
-  type        = number
-}
-
-variable "backend_task_db_name" {
-  description = "Database name for the backend application"
-  type        = string
-}
-
 
 #########
 # RDS
@@ -285,10 +215,4 @@ variable "rds_multi_az" {
   type        = bool
   description = "Specifies if the RDS instance is multi-AZ"
   default     = false
-}
-
-variable "rds_subnet_group_name" {
-  type        = string
-  description = "Name of DB subnet group. DB instance will be created in the VPC associated with the DB subnet group. If unspecified, will be created in the default VPC"
-  default     = "my-subnet-group"
 }
